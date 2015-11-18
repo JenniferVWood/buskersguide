5cb8de60267acc8d3439a503a2e691e25ba6c062
david <david@davidwhoyt.com>
2015-11-18 12:04:47 -0600

    added trigger to manage postgis data on location inserts

--- src/main/java/com/davewhoyt/bg/data/model/Location.java
+++ src/main/java/com/davewhoyt/bg/data/model/Location.java
@@ -1,11 +1,14 @@
 package com.davewhoyt.bg.data.model;
 
+//import org.hibernate.annotations.Type;
+//import org.postgis.Point;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * The giant blob of annotations that is the SqlResultSetMapping is all in order to get
- * an agregate value in each row returned.  Ultimately this will go away, and become
+ * an aggregate value in each row returned.  Ultimately this will go away, and become
  * a cached computed value in the table.
  */
 @Entity
@@ -19,7 +22,9 @@ import java.math.BigDecimal;
                         @FieldResult(name = "locationId", column = "locationid"),
                         @FieldResult(name = "name", column = "name"),
                         @FieldResult(name = "latitude", column = "latitude"),
-                        @FieldResult(name = "longitude", column = "longitude")}),
+                        @FieldResult(name = "longitude", column = "longitude")
+//                        , @FieldResult(name = "geog", column = "geog")
+                }),
         columns = @ColumnResult(name="averageRating", type = Double.class))
 public class Location {
     @Id
@@ -31,6 +36,10 @@ public class Location {
     @Column private String name;
     @Column private BigDecimal latitude;
     @Column private BigDecimal longitude;
+
+//    @Column
+//    @Type(type="org.hibernate.spatial.GeometryType")
+//    private Point geog;
 
     @Transient
     public Double averageRating; // calculated
@@ -99,5 +108,13 @@ public class Location {
     public void setAverageRating(Double averageRating) {
         this.averageRating = averageRating;
     }
+
+//    public Point getGeog() {
+//        return geog;
+//    }
+//
+//    public void setGeog(Point geog) {
+//        this.geog = geog;
+//    }
 }
 
---
+++ src/main/sql/trig_location_post_insert.sql
@@ -0,0 +1,27 @@
+----------------------------------------------------------------------------------------------
+--
+-- fn_trig_location_pre_insert.sql
+--
+-- populate location.geog on each insert.
+--
+-- This exists because mapping that data to/from hibernate is a configuration nightmare,
+-- but we need this type to exist in order to do queries like "find locations close to x,y"
+--
+-----------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION fn_trig_location_pre_insert()
+  RETURNS trigger AS
+$func$
+BEGIN
+
+  NEW.geog := ST_MakePoint(NEW.longitude, NEW.latitude);
+
+  RETURN NEW;
+
+END
+$func$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trig_location_pre_insert
+BEFORE INSERT ON location
+FOR EACH ROW
+EXECUTE PROCEDURE fn_trig_location_pre_insert();
