f781e27e719376e8152999cc1769443a17bbdc7d
david <david@davidwhoyt.com>
2016-08-10 19:42:34 -0500

    permissions fix for acme-challenge

--- src/main/java/com/davewhoyt/bg/TmpController.java
+++ src/main/java/com/davewhoyt/bg/TmpController.java
@@ -13,12 +13,8 @@ public class TmpController {
 
     @RequestMapping("-omQsfC9a4gidM5gCj6RJw5bHoOp-wQxF9_Kc3nUcrs")
     public @ResponseBody String challenge() {
-        return "";
-    }
-
-
-    @RequestMapping("s3jUloJmFiOkMm4wKHJZgMmNzwzxprAhx-BKTZ-NjWI")
-    public @ResponseBody String challenge2() {
         return "-omQsfC9a4gidM5gCj6RJw5bHoOp-wQxF9_Kc3nUcrs.DsbFvnbSxYF93RLG34XfDd0rsqzn-Glc9zhuIEXj13c";
     }
+
+
 }
